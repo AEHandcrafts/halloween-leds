471e7ed27f0820d8a1d2f367a99426a401f4b023
Frazer <yadamsond@gmail.com>
2023-11-04 00:08:20 +0100

    add orange and simple lightning effect

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,30 @@
 #define DATA_PIN 1
 #define CLOCK_PIN 2
 
+int ledCutoff = 154;
+
 CRGB leds[NUM_LEDS];
 
 void setup() {
     FastLED.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 }
 
+int t = 0;
+int color = 0xFF1100;
+
 void loop() {
-    for (int i = 0; i < NUM_LEDS; i++)
-        leds[i] = CRGB::Red;
+    t ++;
+
+    if (t % 100 == 0) {
+        for (int i = 0; i < ledCutoff; i++)
+            leds[i] = CRGB::White;
+    } else {
+        for (int i = 0; i < ledCutoff; i++)
+            leds[i] = CRGB(color);
+    }
+    
+    for (int i = ledCutoff; i < NUM_LEDS; i++)
+        leds[i] = CRGB(color);
     FastLED.show();
-    delay(1000);
+    delay(8);
 }
